1f36e5982acaa7ddb8268114b8c24f4d31f4c6f8
Valeri Zanguri <zanguraa@gmail.com>
2023-09-29 00:39:01 +0400

    feat: added react-router-dom, created routes, and navbar with links

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link> <Link to="/login">Login</Link>{" "}
+        <Link to="/createpost">Create Post</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/createpost" element={<CreatePost />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
